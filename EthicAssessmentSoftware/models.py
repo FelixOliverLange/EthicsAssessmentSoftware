4683b6e23f08442fdbc150660de9f5a9de0da195
Felix Lange <felix.lange@tum.de>
2021-02-07 22:01:13 +0100

    fixing models for migrations, changing primary keys

--- EthicAssessmentSoftware/models.py
+++ EthicAssessmentSoftware/models.py
@@ -4,43 +4,43 @@ from django.db import models
 # ------- UML Layer 1
 
 # Anwendung Class
-class Anwendung(models):
-    id = models.CharField(max_length=70, blank=False, default='')
+class Anwendung(models.Model):
+    name = models.CharField(max_length=70, blank=False, default='', primary_key=True)
 
 # ------- UML Layer 2 and 3
 
 # Motivation class
-class Motivation(models):
-    name = models.CharField(max_length=70, blank=False, default='')
+class Motivation(models.Model):
+    name = models.CharField(max_length=70, blank=False, default='', primary_key=True)
     beschreibung = models.CharField(max_length=200, blank=True, default='')
     shutzklasse = models.CharField(max_length=25, blank=False, default='')
     prioritaet = models.IntegerField(default=1)
     ist_recht = models.BooleanField(default=False)
-    anwendung = models.ForeignKey(to=Anwendung, blank=False, on_delete=models.CASCADE())
+    anwendung = models.ForeignKey(to=Anwendung, blank=False, on_delete=models.CASCADE)
 
 # Stakeholder class
-class Stakeholder(models):
-    name = models.CharField(max_length=70, blank=False, default='')
+class Stakeholder(models.Model):
+    name = models.CharField(max_length=70, blank=False, default='', primary_key=True)
     beschreibung = models.CharField(max_length=200, blank=True, default='')
-    anwendung = models.ForeignKey(to=Anwendung, blank=False, on_delete=models.CASCADE())
+    anwendung = models.ForeignKey(to=Anwendung, blank=False, on_delete=models.CASCADE)
 
 # Konsequenz class
-class Konsequenz(models):
-    name = models.CharField(max_length=70, blank=False, default='')
+class Konsequenz(models.Model):
+    name = models.CharField(max_length=70, blank=False, default='', primary_key=True)
     beschreibung = models.CharField(max_length=200, blank=True, default='')
     bewertung = models.IntegerField()
-    betroffener = models.ForeignKey(to=Stakeholder,on_delete=models.CASCADE())
-    motivation = models.ForeignKey(to=Motivation, on_delete=models.CASCADE())
+    betroffener = models.ForeignKey(to=Stakeholder,on_delete=models.CASCADE)
+    motivation = models.ForeignKey(to=Motivation, on_delete=models.CASCADE)
 
 # Ansatz Class
-class Ansatz(models):
-    name = models.CharField(max_length=70, blank=False, default='')
+class Ansatz(models.Model):
+    name = models.CharField(max_length=70, blank=False, default='', primary_key=True)
     beschreibung = models.CharField(max_length=200, blank=True, default='')
-    adressiert = models.ForeignKey(to=Konsequenz,on_delete=models.CASCADE())
-    anwendung = models.ForeignKey(to=Anwendung, blank=False, on_delete=models.CASCADE())
+    adressiert = models.ForeignKey(to=Konsequenz,on_delete=models.CASCADE)
+    anwendung = models.ForeignKey(to=Anwendung, blank=False, on_delete=models.CASCADE)
 
 # Anforderung class
-class Anforderung(models):
-    name = models.CharField(max_length=70, blank=False, default='')
+class Anforderung(models.Model):
+    name = models.CharField(max_length=70, blank=False, default='', primary_key=True)
     beschreibung = models.CharField(max_length=200, blank=True, default='')
-    ansatz = models.ForeignKey(to=Ansatz, on_delete=models.CASCADE())
+    ansatz = models.ForeignKey(to=Ansatz, on_delete=models.CASCADE)
