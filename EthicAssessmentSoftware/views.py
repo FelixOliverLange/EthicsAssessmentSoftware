ca0667019ba4c91dca9cc699a7a82f63cfadb66f
Felix Lange <felix.lange@tum.de>
2021-02-10 13:36:24 +0100

    adding anwendung_stakeholder_list

--- EthicAssessmentSoftware/views.py
+++ EthicAssessmentSoftware/views.py
@@ -27,7 +27,7 @@ def anwendung_list(request):
         if anwendung_serializer.is_valid():
             anwendung_serializer.save()
             # Theoretically one could respond with the data sent. We don't do so here because of reflection attacks
-            return JsonResponse('',status=status.HTTP_201_CREATED)
+            return JsonResponse({},status=status.HTTP_201_CREATED)
         else:
             # This should NOT return the errors to not reveal internal server errors. Instead this SHOULD be logged. But this is the insecure first version.
             return JsonResponse(AnwendungSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -46,7 +46,7 @@ def anwendung_list(request):
 def anwendung_details(request, anwendung_name):
     # I assume this to work via 'name'. Potentially some more DB-specific field name is requried, most docs use pk
     try:
-        anwendung = Anwendung.objects.get(name=anwendung_name)
+        anwendung = Anwendung.objects.get(pk=anwendung_name)
     except Anwendung.DoesNotExist:
         return JsonResponse({'message': 'the requested object does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
@@ -62,7 +62,7 @@ def anwendung_details(request, anwendung_name):
         if anwendung_serializer.is_valid():
             anwendung_serializer.save()
             # Theoretically one could respond with the data sent. We don't do so here because of reflection attacks
-            return JsonResponse('', status=status.HTTP_201_CREATED)
+            return JsonResponse({}, status=status.HTTP_201_CREATED)
         else:
             # This should NOT return the errors to not reveal internal server errors. Instead this SHOULD be logged. But this is the insecure first version.
             return JsonResponse(AnwendungSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -75,3 +75,36 @@ def anwendung_details(request, anwendung_name):
     # block for any other methods. This should be blocked by the api_view spec, but as a doc / security measure:
     else:
         return JsonResponse('', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+# endpoint for Stakeholder
+@api_view(['GET','POST','DELETE'])
+def anwendung_stakeholder_list(request, anwendung_name):
+    # I assume this to work via 'name'. Potentially some more DB-specific field name is requried, most docs use pk
+    # This is only done here to catch cases where the declared Anwendung does not exist
+    try:
+        anwendung_object = Anwendung.objects.get(pk=anwendung_name)
+    except Anwendung.DoesNotExist:
+        return JsonResponse({'message': 'the requested object does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    # GET for Stakeholder
+    if request.method == 'GET':
+        # get Stakeholder via "Find all objects by condition"
+        stakeholders = Stakeholder.objects.filter(pk = anwendung_name)
+        stakeholder_serializer = StakeholderSerializer(stakeholders, many=True)
+        return JsonResponse(StakeholderSerializer.data, status=status.HTTP_200_OK, safe=False)
+
+    # POST for Stakeholder
+    # TODO: Potentially, anwendung_name should be used as part of this as well to be inserted before serialization
+    # For that, see https://sunscrapers.com/blog/the-ultimate-tutorial-for-django-rest-framework-functional-endpoints-and-api-nesting-part-6/
+    if request.method == 'POST':
+        stakeholder_data = JSONParser().parse(request)
+        stakeholder_serializer = StakeholderSerializer(data=stakeholder_data)
+        if stakeholder_serializer.is_valid():
+            stakeholder_serializer.save()
+            return JsonResponse({},status=status.HTTP_201_CREATED)
+
+    # Delete for Stakeholders with condition
+    if request.method == 'DELETE':
+        # get Stakeholder via "Find all objects by condition"
+        Stakeholder.objects.filter(anwendung=anwendung_name).delete()
+        return JsonResponse({'message':'objects deleted'}, status=status.HTTP_200_OK)
