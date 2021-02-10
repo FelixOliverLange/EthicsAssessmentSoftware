ad4bb63913d58f811b9338106dc114dc7f2ca5de
Felix Lange <felix.lange@tum.de>
2021-02-10 14:00:34 +0100

    adjusting queries and adding anwendung_stakeholder_details

--- EthicAssessmentSoftware/views.py
+++ EthicAssessmentSoftware/views.py
@@ -11,6 +11,10 @@ from EthicAssessmentSoftware.serializers import *
 # source: https://bezkoder.com/django-crud-mysql-rest-framework/#:~:text=First%2C%20we%20setup%20Django%20Project,operations%20(including%20custom%20finder).
 # TODO: potentially include forms from https://www.laravelcode.com/post/django-3-crud-tutorial-example-with-mysql-and-bootstrap
 
+# For composition of field lookups inside get() and filter(), see
+# https://docs.djangoproject.com/en/3.1/topics/db/queries/#field-lookups
+# TODO: Potentially rework GET requests fro _list methods to respect 404
+
 # endpoint for Anwendung
 @api_view(['GET','POST','DELETE'])
 def anwendung_list(request):
@@ -39,14 +43,14 @@ def anwendung_list(request):
 
     # block for any other methods. This should be blocked by the api_view spec, but as a doc / security measure:
     else:
-        return JsonResponse('',status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return JsonResponse({},status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 # endpoint for Anwendung details
 @api_view(['GET', 'PUT', 'DELETE'])
 def anwendung_details(request, anwendung_name):
     # I assume this to work via 'name'. Potentially some more DB-specific field name is requried, most docs use pk
     try:
-        anwendung = Anwendung.objects.get(pk=anwendung_name)
+        anwendung = Anwendung.objects.get(name=anwendung_name)
     except Anwendung.DoesNotExist:
         return JsonResponse({'message': 'the requested object does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
@@ -74,7 +78,7 @@ def anwendung_details(request, anwendung_name):
 
     # block for any other methods. This should be blocked by the api_view spec, but as a doc / security measure:
     else:
-        return JsonResponse('', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return JsonResponse({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 # endpoint for Stakeholder
 @api_view(['GET','POST','DELETE'])
@@ -82,21 +86,21 @@ def anwendung_stakeholder_list(request, anwendung_name):
     # I assume this to work via 'name'. Potentially some more DB-specific field name is requried, most docs use pk
     # This is only done here to catch cases where the declared Anwendung does not exist
     try:
-        anwendung_object = Anwendung.objects.get(pk=anwendung_name)
+        anwendung_object = Anwendung.objects.get(name=anwendung_name)
     except Anwendung.DoesNotExist:
         return JsonResponse({'message': 'the requested object does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
     # GET for Stakeholder
     if request.method == 'GET':
         # get Stakeholder via "Find all objects by condition"
-        stakeholders = Stakeholder.objects.filter(pk = anwendung_name)
+        stakeholders = Stakeholder.objects.filter(anwendung__name=anwendung_name)
         stakeholder_serializer = StakeholderSerializer(stakeholders, many=True)
         return JsonResponse(StakeholderSerializer.data, status=status.HTTP_200_OK, safe=False)
 
     # POST for Stakeholder
     # TODO: Potentially, anwendung_name should be used as part of this as well to be inserted before serialization
     # For that, see https://sunscrapers.com/blog/the-ultimate-tutorial-for-django-rest-framework-functional-endpoints-and-api-nesting-part-6/
-    if request.method == 'POST':
+    elif request.method == 'POST':
         stakeholder_data = JSONParser().parse(request)
         stakeholder_serializer = StakeholderSerializer(data=stakeholder_data)
         if stakeholder_serializer.is_valid():
@@ -104,7 +108,46 @@ def anwendung_stakeholder_list(request, anwendung_name):
             return JsonResponse({},status=status.HTTP_201_CREATED)
 
     # Delete for Stakeholders with condition
-    if request.method == 'DELETE':
+    elif request.method == 'DELETE':
         # get Stakeholder via "Find all objects by condition"
-        Stakeholder.objects.filter(anwendung=anwendung_name).delete()
+        Stakeholder.objects.filter(anwendung__name=anwendung_name).delete()
         return JsonResponse({'message':'objects deleted'}, status=status.HTTP_200_OK)
+
+    # block for any other methods. This should be blocked by the api_view spec, but as a doc / security measure:
+    else:
+        return JsonResponse({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+# endpoint for Stakeholder details
+@api_view(['GET', 'PUT', 'DELETE'])
+def anwendung_stakeholder_details(request, anwendung_name, stakeholder_name):
+    # get all appliceable stakeholders first
+    try:
+        stakeholders = Stakeholder.objects.filter(name=stakeholder_name, anwendung__name=anwendung_name)
+    except Stakeholder.DoesNotExist:
+        return JsonResponse({'message':'no matching stakeholder for name and application found'}, status=status.HTTP_404_NOT_FOUND)
+
+    # GET for Stakeholder with condition
+    if request.method == 'GET':
+        stakeholder_serializer = StakeholderSerializer(stakeholders)
+        return JsonResponse(stakeholder_serializer.data, status=status.HTTP_200_OK)
+
+    # PUT for Stakeholder
+    elif request.method == 'PUT':
+        stakeholder_data = JSONParser.parse(request)
+        stakeholder_serializer = StakeholderSerializer(stakeholders, data=stakeholder_data)
+        if stakeholder_serializer.is_valid():
+            stakeholder_serializer.save()
+            # Theoretically one could respond with the data sent. We don't do so here because of reflection attacks
+            return JsonResponse({}, status=status.HTTP_201_CREATED)
+        else:
+            # This should NOT return the errors to not reveal internal server errors. Instead this SHOULD be logged. But this is the insecure first version.
+            return JsonResponse(StakeholderSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # DELETE for Stakeholder with conditions
+    elif request.method == 'DELETE':
+        stakeholders.delete()
+        return JsonResponse({'message': 'Stakeholder was deleted successfully!'}, status=status.HTTP_200_OK)
+
+    # block for any other methods. This should be blocked by the api_view spec, but as a doc / security measure:
+    else:
+        return JsonResponse({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
