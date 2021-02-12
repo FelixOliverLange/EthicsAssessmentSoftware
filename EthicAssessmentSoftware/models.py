179d7e6b73168d09d589bc066ec5d90a90893789
Felix Lange <felix.lange@tum.de>
2021-02-12 03:10:16 +0100

    adding str method for better UX in admin interface

--- EthicAssessmentSoftware/models.py
+++ EthicAssessmentSoftware/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class Anwendung(models.Model):
     name = models.CharField(max_length=70, blank=False, default='', unique=True)
 
+    def __str__(self):
+        return self.name
+
 # ------- UML Layer 2 and 3
 
 # Motivation class
@@ -18,11 +21,17 @@ class Motivation(models.Model):
     ist_recht = models.BooleanField(default=False)
     anwendung = models.ForeignKey(to=Anwendung, blank=False, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 # Stakeholder class
 class Stakeholder(models.Model):
     name = models.CharField(max_length=70, blank=False, default='', unique=True)
     beschreibung = models.CharField(max_length=200, blank=True, default='')
     anwendung = models.ForeignKey(to=Anwendung, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 # Konsequenz class
 class Konsequenz(models.Model):
@@ -32,6 +41,9 @@ class Konsequenz(models.Model):
     betroffener = models.ForeignKey(to=Stakeholder,on_delete=models.CASCADE)
     motivation = models.ForeignKey(to=Motivation, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 # Ansatz Class
 class Ansatz(models.Model):
     name = models.CharField(max_length=70, blank=False, default='', unique=True)
@@ -40,8 +52,14 @@ class Ansatz(models.Model):
     auswirkung = models.IntegerField(default=0)
     anwendung = models.ForeignKey(to=Anwendung, blank=False, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 # Anforderung class
 class Anforderung(models.Model):
     name = models.CharField(max_length=70, blank=False, default='', unique=True)
     beschreibung = models.CharField(max_length=200, blank=True, default='')
     ansatz = models.ForeignKey(to=Ansatz, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
