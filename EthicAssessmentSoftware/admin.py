7bd1559b8a90a0a51b4ece6961f15224fad98fd1
Felix Lange <felix.lange@tum.de>
2021-02-12 02:59:13 +0100

    added admin views for all datamodels

--- EthicAssessmentSoftware/admin.py
+++ EthicAssessmentSoftware/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from EthicAssessmentSoftware.models import *
 
 # Register your models here.
+admin.site.register(Anwendung)
+admin.site.register(Motivation)
+admin.site.register(Stakeholder)
+admin.site.register(Konsequenz)
+admin.site.register(Ansatz)
+admin.site.register(Anforderung)
